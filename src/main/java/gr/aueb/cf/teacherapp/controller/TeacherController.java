1357ea6b467c845a5272798e9854a65b04e4ab37
konbarbounis <konbarbou@gmail.com>
2024-10-21 22:24:13 +0300

    20241021 added ui (html templates and css)

--- src/main/java/gr/aueb/cf/teacherapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/teacherapp/controller/TeacherController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -37,6 +38,7 @@ public class TeacherController {
         return "teacher-form";
     }
 
+    @PostMapping("/teachers/insert")
     public String saveTeacher(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO insertDTO,
                               BindingResult bindingResult, Model model){
 
